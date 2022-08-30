bbfe06b3756f03f799bb45ce03d4a376555558ab
0x44F <89411010+0x44F@users.noreply.github.com>
2022-08-30 15:45:43 +1200

    Limit length (or it won't work)

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ from steganography.steganography import Steganography as stego
 PAYLOAD     =   'alert(0);'      # Insert your payload here, example 'alert(0);'
 CHARCODE    =   '\x00\x32\x38\xF1\xA3\xC7\xF4\x00\xFF\xFF\xFF'
 
+MAX_LEN     =   128
+
 if __name__ == '__main__':
+    if(len(payload) > 128)
+        quit("Payload too long!")
+    
     target_image = input("Target Image Path > ")     # Grab target image (meow)
     output_path = input("Output Path > ")            # Get path to save image (e.g: C:\Documents\my_hacked_image.jpg)
     
