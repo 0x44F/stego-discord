05a6d06a62e2df639eff3ab130ec747b3554f756
TheOnlyWayUp <thedarkdraconian@gmail.com>
2022-08-30 08:21:25 +0000

    fix(main): Fixed errors and modified Steganography library
    - Removed from future imports
    - Fixed minor errors
    - Updated script to use MAX_LEN Variable rather than hardcoding 128
    - Updated steganography to disable output check, modified hex encoding functions

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-# Author: 0x44F
+from steganography import Steganography as stego
 
-from __future__ import absolute_import, unicode_literals
-from steganography.steganography import Steganography as stego
+PAYLOAD = "alert(0);"  # Insert your payload here, example 'alert(0);'
+CHARCODE = "\x00\x32\x38\xF1\xA3\xC7\xF4\x00\xFF\xFF\xFF"
 
-PAYLOAD     =   'alert(0);'      # Insert your payload here, example 'alert(0);'
-CHARCODE    =   '\x00\x32\x38\xF1\xA3\xC7\xF4\x00\xFF\xFF\xFF'
+MAX_LEN = 128
 
-MAX_LEN     =   128
-
-if __name__ == '__main__':
-    if(len(payload) > 128):
+if __name__ == "__main__":
+    if len(PAYLOAD) > MAX_LEN:
         quit("Payload too long!")
-    
-    target_image = input("Target Image Path > ")     # Grab target image (meow)
-    output_path = input("Output Path > ")            # Get path to save image (e.g: C:\Documents\my_hacked_image.jpg)
-    
+
+    target_image = input("Target Image Path > ")  # Grab target image (meow)
+    output_path = input(
+        "Output Path > "
+    )  # Get path to save image (e.g: C:\Documents\my_hacked_image.jpg)
+
     stego.encode(target_image, output_path, PAYLOAD + str(CHARCODE))
     print("Finished process! [READ TEXT TO SEE IF ITS CORRECT]")
     print("We found your payload! It was: {}".format(str(stego.decode(output_path))))
-    
-    print("If the above text is [incorrect] please try again using path you have access to.")
-    return
+
+    print(
+        "If the above text is [incorrect] please try again using path you have access to."
+    )
