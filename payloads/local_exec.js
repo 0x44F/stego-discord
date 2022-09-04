d08e3abe5513d5a1eb1ee2edfbd75c2cde5a7e98
0x44F <89411010+0x44F@users.noreply.github.com>
2022-09-04 22:09:59 +1200

    Create local_exec.js

---
+++ payloads/local_exec.js
@@ -0,0 +1,7 @@
+var shell = WScript.CreateObject("WScript.Shell");
+shell.Run("command here");
+
+/*
+    Locally execute shell commands on Windows systems, note this will have to override
+    index.js (not used in some new version)
+*/
